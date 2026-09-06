ca0d60ad9733d36cc75f9fe5c67ca8f61f54aa09
gustavo-kissoka <guguplaergustavo@gmail.com>
2026-09-06 23:26:54 +0100

    atualização do código

--- dia-058-sistema-de-produtos/assets/script.js
+++ dia-058-sistema-de-produtos/assets/script.js
@@ -1,17 +1,17 @@
 const modalAdicionar = document.getElementById("modal-adicionar");
 const modalEditar = document.getElementById("modal-editar");
 
-// Abrir Modal Adicionar
+
 document.getElementById("btn-open-adicionar").addEventListener("click", () => {
   modalAdicionar.classList.add("active");
 });
 
-// Abrir Modal Editar + Autocompletar Inputs
+
 document.querySelectorAll(".btn-open-editar").forEach((btn) => {
   btn.addEventListener("click", (e) => {
     const target = e.currentTarget;
 
-    // Puxa os dados armazenados nos atributos 'data-*' do botão e preenche o modal
+  
     document.getElementById("edit-id").value = target.getAttribute("data-id");
     document.getElementById("edit-nome").value =
       target.getAttribute("data-nome");
@@ -26,7 +26,7 @@ document.querySelectorAll(".btn-open-editar").forEach((btn) => {
   });
 });
 
-// Fechar Modais
+
 document.querySelectorAll(".close-modal").forEach((btn) => {
   btn.addEventListener("click", () => {
     modalAdicionar.classList.remove("active");
